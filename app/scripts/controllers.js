1fab66039a03dcfa9eb4f5e5a0b894076beada7b
lukas.parobok <lukas.parobok@accenture.com>
2016-04-01 20:51:04 +0200

    new tasks

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -28,8 +28,19 @@ biometricsControllers.controller('FileEnrollCtrl', ['$scope', 'Faces',
 biometricsControllers.controller('ListCtrl', ['$scope', 'Faces',
     function ($scope, Faces) {
         $scope.listFaces = function () {
-            // TODO: warm-up no.1 - get the list of faces from back-end and assign it to appropriate $scope variable
-            $scope.faces = Faces.get();
+            $scope.isDisabled = true;
+
+            // TODO: Task no.1.1 - get the list of faces from back-end and assign it to appropriate $scope variable
+            $scope.faces = Faces.get({},function(){
+                // TODO: Task no.1.2 - In 'Life capture' screen 'identify' button is disabled untill we get the data back from server - do the same for 'list' button in 'List' screen
+                $scope.isDisabled = false;
+            });
+
+            // TODO: Task no.1.3 currently the list of names is out of bounds - in appropriate template decide which tag to use to fit inside of borders
+
+            // solution - swap 'span' for 'label' in list.html
+
+
         }
     }]);
 
@@ -37,7 +48,7 @@ biometricsControllers.controller('DeleteCtrl', ['$scope', 'User',
     function ($scope, User) {
         $scope.deleteUser = function () {
             User.delete({id: $scope.deleteName}, function (response) {
-                // TODO: warm-up no.1 - item is being deleted - assign response to appropriate $scope variable and show the message in template
+                // TODO: Task no.2.1 - item is being deleted - assign response to appropriate $scope variable and show the message in template
                 $scope.response = response;
                 $scope.showStatus = true;
             });
@@ -47,10 +58,13 @@ biometricsControllers.controller('DeleteCtrl', ['$scope', 'User',
 biometricsControllers.controller('PictureCtrl', ['$scope',
     function ($scope) {
         $scope.getPic = function () {
-            //TODO: warm-up no.2 - get the name from text input and use it to assemble path to picture; assign the assembled path to appropriate $scope variable so the picture would be viewed in template
+            //TODO: Task no.2.2 - get the name from text input and use it to assemble path to picture; assign the assembled path to appropriate $scope variable so the picture would be viewed in template
             var name = $scope.picName;
 
             $scope.path = '/openbr/faces/' + name + '.jpg';
+            
+            //TODO: Task no 2.3 - images loaded are out of bounds, make the img responsive
+            // solution : class="img-responsive"
         }
     }]);
 
@@ -58,15 +72,25 @@ biometricsControllers.controller('LiveCaptureCtrl', ['$scope', 'Identify', 'Face
     function ($scope, Identify, Faces) {
 
         $scope.identify = function () {
-            // TODO: task1: sort response either here or directly in service
+
             $scope.isDisabled = true;
+            $scope.showTaken = true; //task 3.2
 
             var data = Identify.post({image: $scope.getData()}, function () {
                 $scope.isDisabled = false;
+
+                // TODO: task 3.1 - sort the response according to score
                 var sorted = data.sort(function (a, b) {
                     return a.score - b.score;
                 });
                 $scope.matches = sorted.reverse();
+
+                //TODO: task 3.2 - show most similar image
+
+                $scope.showTaken = false;
+
+                $scope.similarPath = '/openbr/faces/' + $scope.matches[0].name + '.jpg';
+                $scope.showSimilar = true;
             });
         };
 
