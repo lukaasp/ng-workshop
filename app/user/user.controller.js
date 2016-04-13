b9b1abf74b0d2ee37f7a6b2cc5adae203b69fe98
lukas.parobok <lukas.parobok@accenture.com>
2016-04-13 15:39:27 +0200

    added task to deleteUser

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -1,36 +1,44 @@
-(function() {
+(function () {
 
-  angular
-    .module('user')
-    .controller('UserCtrl', UserController);
+    angular
+        .module('user')
+        .controller('UserCtrl', UserController);
 
-  function UserController($scope, User) {
-    $scope.listFaces = function() {
-      $scope.isDisabled = true;
+    function UserController($scope, User) {
+        $scope.listFaces = function () {
+            $scope.isDisabled = true;
 
-      // TODO: Task no.1.1 - get the list of faces from back-end and assign it to appropriate $scope variable
-      $scope.faces = User.get({},function() {
-        // TODO: Task no.1.2 - In 'Life capture' screen 'identify' button is disabled
-        // untill we get the data back from server - do the same for 'list' button in 'List' screen
-        $scope.isDisabled = false;
-      });
+            // TODO: Task no.1.1 - get the list of faces from back-end and assign it to appropriate $scope variable
+            $scope.faces = User.get({}, function () {
+                // TODO: Task no.1.2 - In 'Life capture' screen 'identify' button is disabled
+                // untill we get the data back from server - do the same for 'list' button in 'List' screen
+                $scope.isDisabled = false;
+            });
 
-      // TODO: Task no.1.3 currently the list of names is out of bounds
-      // in appropriate template decide which tag to use to fit inside of borders
+            // TODO: Task no.1.3 currently the list of names is out of bounds
+            // in appropriate template decide which tag to use to fit inside of borders
 
-      // solution - swap 'span' for 'label' in list.html
+            // solution - swap 'span' for 'label' in list.html
 
-    };
-    $scope.deleteUser = function() {
-      User.delete({id: $scope.deleteName}, function(response) {
-        // TODO: Task no.3.1 - item is being deleted - assign response to appropriate $scope variable
-        // and show the message in template
-        $scope.response = response;
-        $scope.showStatus = true;
-      });
+        };
+        $scope.deleteUser = function () {
+            $scope.showStatus = false;
+            $scope.showError = false;
 
-      // TODO: Task no.3.2 - two-step delete - on 'delete' button click show yes/no buttons,
-      // only after clicking 'yes' button proceed with delete
-    };
-  }
+            User.delete({id: $scope.deleteName}, function (response) {
+                // TODO: Task no.3.1 - item is being deleted - assign response to appropriate $scope variable
+                // and show the message in template
+                $scope.response = response;
+                $scope.showStatus = true;
+
+                // TODO: Task no.3.1 - catch error response
+            }, function (error) {
+                $scope.errorResponse = error.data;
+                $scope.showError = true;
+            });
+
+            // TODO: Task no.3.3 - two-step delete - on 'delete' button click show yes/no buttons,
+            // only after clicking 'yes' button proceed with delete
+        };
+    }
 })();
