4d50c1b5908b04347106a78252084139ab8bbd29
zuzana.harcarikova <zuzana.harcarikova@accenture.com>
2016-04-26 22:12:04 +0200

    updatd comparison.controller.js

--- app/comparison/comparison.controller.js
+++ app/comparison/comparison.controller.js
@@ -11,10 +11,16 @@
       $scope.comparisons = [];
       $scope.image1 = {name: 'Albert Einstein', file: 'albert_einstein.jpg'};
       $scope.image2 = {name: 'Emma Watson', file: 'emma_watson.jpg'};
-      //
+      $scope.lastResult = null;
+
+      // TODO: 5.2 Select user to compare
+      //           On click on thumbnail image, set second compared image as first
+      //           and clicked image as second
       $scope.setImageForComparison = function(name, image) {
       };
 
+      // TODO: Task 5.1 Compare two users
+      // HINT: use `compare` from `comparison.factory.js`
       $scope.compare = function() {
       };
     }
