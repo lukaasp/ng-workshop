ddb7b707d0cb6536c30915c77f4df3b6af61cc8c
zuzana.harcarikova <zuzana.harcarikova@accenture.com>
2016-04-26 13:26:14 +0200

    tasks 1.1, 1.2

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -5,45 +5,21 @@
       .controller('UserCtrl', UserController);
 
   function UserController($scope, User) {
-    // TODO: Task no.1.1 - get the list of faces from back-end and assign it to appropriate $scope variable, make the list load at start
+    // TODO: Task no.1.1:
+    //       Get the list of faces from back-end
+    //       and assign it to appropriate $scope variable, make the list load at start
+    // HINT: https://docs.angularjs.org/api/ng/service/$http
+    //       https://docs.angularjs.org/api/ng/directive/ngClick
+    $scope.faces = [];
+    $scope.listAll = listAll;
 
-    /*$scope.listAll = listAll();
-    listAll();
+    // TODO: Task no.1.2
+    //       Disable 'List' button until we get data back from server
+    // HINT: https://docs.angularjs.org/api/ng/directive/ngDisabled
+    $scope.isDisabled = null;
 
     function listAll() {
-      $scope.isDisabled = true;
 
-      // TODO: Task no.1.2 - disable 'List' button until we get data back from server
-
-      User.syncWithRemote().then(function(faces) {
-        $scope.faces = faces;
-        $scope.isDisabled = false;
-      }, function(error) {
-        console.log('Could not sync with remote server.');
-        console.log(error);
-        $scope.faces = [];
-        $scope.isDisabled = false;
-      });
-    }*/
-
-    $scope.deleteUser = function(nameToDelete) {
-      $scope.showStatus = false;
-      $scope.showError = false;
-
-      User.deleteUser(nameToDelete).then(function(response) {
-        // TODO: Task no.3.1 - item is being deleted - assign response to appropriate $scope variable and show the message in template
-        /*$scope.response = response;
-        $scope.showStatus = true;*/
-        // TODO: Task no.3.3 - catch error response
-      }/*, function(error) {
-        // TODO: Task no 3.4 - use toastr to add some funny error message
-        toastr.warning('','',{closeButton:true,closeHtml:'<img src="/pics/no.jpg" height="200" width="200">'});
-        $scope.errorResponse = error.data;
-        $scope.showError = true;
-      }*/);
-
-      // TODO: Task no.3.5 - two-step delete - on 'delete' button click show yes/no buttons,
-      // only after clicking 'yes' button proceed with delete
-    };
+    }
   }
 })();
